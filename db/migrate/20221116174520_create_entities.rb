3296d346b576fd3f0b8e1e15f220086275d3a9c7
tanveerisonline <tanveerisonline@gmail.com>
2023-04-29 15:39:43 +0530

    Add migrations for entities ,devise and groups_entities

--- db/migrate/20221116174520_create_entities.rb
+++ db/migrate/20221116174520_create_entities.rb
@@ -1,11 +1,11 @@
-# class CreateEntities < ActiveRecord::Migration[7.0]
-#   def change
-#     create_table :entities do |t|
-#       t.string :name
-#       t.integer :amount
-#       t.references :user, null: false, foreign_key: true
+class CreateEntities < ActiveRecord::Migration[7.0]
+  def change
+    create_table :entities do |t|
+      t.string :name
+      t.integer :amount
+      t.references :user, null: false, foreign_key: true
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
