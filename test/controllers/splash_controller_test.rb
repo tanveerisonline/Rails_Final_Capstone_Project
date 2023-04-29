1cfbb1dc4bbcf280c1921eca1f97fe70f0f13537
tanveerisonline <tanveerisonline@gmail.com>
2023-04-29 15:49:57 +0530

    Add controller specs for entities_controller,groups_controller and splash_controller

--- test/controllers/splash_controller_test.rb
+++ test/controllers/splash_controller_test.rb
@@ -1,7 +1,7 @@
-# require 'test_helper'
+require 'test_helper'
 
-# class SplashControllerTest < ActionDispatch::IntegrationTest
-#   # test "the truth" do
-#   #   assert true
-#   # end
-# end
+class SplashControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
